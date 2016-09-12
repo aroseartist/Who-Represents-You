5fad1f218da17f4abed0ad7eaf952fe4e7fb3f45
Lora Rose <aroseartist@gmail.com>
2016-09-12 12:36:40 -0700

    Heroic setup

--- server.py
+++ server.py
@@ -166,5 +166,9 @@ if __name__ == "__main__":
     # connect_to_db(app)
 
     # Run app locally
-    app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0')
+    
+    #  Run app via Heroku
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
 
